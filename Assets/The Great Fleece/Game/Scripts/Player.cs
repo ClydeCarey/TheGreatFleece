5d85c631d2db0c93a1b227d2ce99fed89aefb635
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-12-08 08:35:44 -1000

    tempcommit
    tempcommit

---
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    //create a variable to hold our navmesh agent
+    //handle to navmesh agent
+    private NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //assign handle to the navmesh component
+        _agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                Debug.Log("Hit: " + hitInfo.point);
+                //handle.destination = hitinfo position
+                //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //cube.transform.position = hitInfo.point;
+            }
+        }
+        //cast a ray from mouse position
+        //debug floor position hit
+        //create object at floor position
+    }
+}
