e21009aaf48a4d07e22755df915c7bc37ad3438b
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-12-09 11:06:37 -1000

    mergingMovingSloppy
    mergingMovingSloppy

--- Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -1,13 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardAI : MonoBehaviour
 {
+<<<<<<< Updated upstream
     
+=======
+
+    public List<Transform> wayPoints;
+
+    public Transform currentTarget;
+
+    private NavMeshAgent _agent;
+>>>>>>> Stashed changes
     void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
+
+        if (wayPoints.Count > 0)
+        {
+            if (wayPoints[0] != null)
+            {
+                currentTarget = wayPoints[0];
+                _agent.SetDestination(currentTarget.position);
+            }            
+        }        
     }
 
     // Update is called once per frame
