5e6f8cdc5a00d8afb77fa394d86462844956c4cf
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-12-09 15:46:46 -1000

    Missing darren
    Missing darren

--- Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -30,24 +30,47 @@ public class GuardAI : MonoBehaviour
 
             if (distance < 1.0f && _targetReached == false)
             {
-                _targetReached = true;                
-                Debug.Log("target reached" + _targetReached);
+                if ((_currentTarget == 0 || _currentTarget == wayPoints.Count - 1))
+                {
+                    _targetReached = true;
+                    //Debug.Log("target reached" + _targetReached);
+                    StartCoroutine(WaitBeforeMoving());
+                }
+                else
+                {
+                    if (_reverse == true)
+                    {
+                        _currentTarget--;
+                    }
+                    else
+                    {
+                        _currentTarget++;
+                    }
+                }
 
-                StartCoroutine(WaitBeforeMoving());                             
-                
-            }            
+            }
+            
         }
     }
 
     IEnumerator WaitBeforeMoving()
     {
-        Debug.Log("waitBeforeMoving");
-        yield return new WaitForSeconds(2.0f);
+        //Debug.Log("waitBeforeMoving");
+
+        if (_currentTarget == 0)
+        {
+            yield return new WaitForSeconds(2.0f);    //(Random.Range(2.0f, 5.0f));
+        }
+        else if (_currentTarget == wayPoints.Count - 1)
+        {
+            yield return new WaitForSeconds(2.0f);   //(Random.Range(2.0f, 5.0f));
+        }
+                     
 
         if (_reverse == true)
         {
             _currentTarget--;
-
+            Debug.Log("ienum reverse true target--");
             if (_currentTarget == 0)
             {
                 _reverse = false;
@@ -58,12 +81,12 @@ public class GuardAI : MonoBehaviour
         else if (_reverse == false)
         {
             _currentTarget++;
-
+            Debug.Log("ienum reverse true target++");
             if (_currentTarget == wayPoints.Count)
             {
                 _reverse = true;
                 _currentTarget--;
-                
+                Debug.Log("ienum reverse false targetwaypoints count target--");
             }
 
         }
