fe00f02567646b31ac8ea3167ca684b135f58cf2
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-12-09 20:56:33 -1000

    rebuilding
    rebuilding

--- Assets/The Great Fleece/Game/Scripts/CameraTrigger.cs
+++ Assets/The Great Fleece/Game/Scripts/CameraTrigger.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class CameraTrigger : MonoBehaviour
 {
-    
+    public Transform myCamera;
+
     //check foir trigger when player crosses
     //update main camera to the triggers camera progression angle
 
     //check for trigger
     //debug.log trigger activated
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("trigger activated");
+            Camera.main.transform.position = myCamera.transform.position;
+            Camera.main.transform.rotation = myCamera.transform.rotation;
+        }
+    }
 
 }
