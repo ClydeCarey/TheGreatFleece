d96b10ae5ec403421f584ea7267f247509e69425
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-12-11 07:27:46 -1000

    cointoss
    cointoss

---
+++ Assets/The Great Fleece/Game/Scripts/Eyes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour
+{
+    //create ability to detect darren if he's caught in the eye
+
+    //void ontrigger enter
+    //check for player tag
+    //enable gameover cutscene
+    public GameObject gameOverCutscene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gameOverCutscene.SetActive(true);
+        }
+    }
+}
