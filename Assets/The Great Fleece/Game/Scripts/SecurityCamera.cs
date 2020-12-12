d0c1619c035a04f469a8f03ab7d42d41eb42d8e3
ClydeCarey <clyde@gamedevhq.com>
2020-12-11 19:48:19 -1000

    starting security cameras
    starting security cameras

---
+++ Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCamera : MonoBehaviour
+{
+    public GameObject gameOverCutscene;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gameOverCutscene.SetActive(true);
+        }
+    }
+}
