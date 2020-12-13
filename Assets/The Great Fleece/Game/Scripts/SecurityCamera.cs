2fa65af2ffe998dcec81200e766611a67361cea9
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-12-13 12:58:17 -1000

    SleepingGuardCutscene
    SleepingGuardCutscene

--- Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
+++ Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class SecurityCamera : MonoBehaviour
 {
     public GameObject gameOverCutscene;
+
+    //get a handle to the animator 
+    public Animator anim;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            gameOverCutscene.SetActive(true);
+            MeshRenderer render = GetComponent<MeshRenderer>();
+            Color color = new Color(0.06f, 0.1f, 0.1f, 0.3f); // 154, 29, 29, 10
+            render.material.SetColor("_TintColor", color);
+            anim.enabled = false;
+            StartCoroutine(AlertRoutine());
+
         }
+    }
+
+    IEnumerator AlertRoutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        gameOverCutscene.SetActive(true);
     }
 }
