7735fa8ff12f8eab5d95361e855cdcd819453d74
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-12-14 12:21:45 -1000

    fix nav mesh
    fix nav mesh

---
+++ Assets/The Great Fleece/Game/Scripts/GrabKeycardActivation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabKeycardActivation : MonoBehaviour
+{
+    public GameObject sleepingGuardCutscene;
+    private bool _sceneHasBeenTriggered = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && _sceneHasBeenTriggered == false)
+        {
+            sleepingGuardCutscene.SetActive(true);
+            _sceneHasBeenTriggered = true;
+        }
+    }
+}
