aa51a32207782f4ab9524ec2d16b5dff31521e6f
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-12-18 10:05:09 -1000

    Finished
    Finished but now go and tweak everything

---
+++ Assets/The Great Fleece/Game/Scripts/Menu/LoadLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public Image progressBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //call coroutine to load main scene
+        StartCoroutine(LoadLevelAsync());
+    }
+
+    IEnumerator LoadLevelAsync()
+    {
+        
+
+        //create an async operation = loadsceneasync("main")
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Main");
+
+        //while operation is ongoing
+        while (operation.isDone == false)
+        {
+            //progress bar fill = operation progress
+            progressBar.fillAmount = operation.progress;
+            //yield wait for end of frame
+            yield return new WaitForEndOfFrame();
+        }       
+        
+    }
+}
---
+++ Assets/The Great Fleece/Game/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // start lunuches the game
+    public void StartGame()
+    {
+        Debug.Log("pressed start");
+        SceneManager.LoadScene("Loading Screen");
+    }
+
+    //quit quits the game (only works on build)
+    public void Quit()
+    {
+        Debug.Log("pressed quit");
+        Application.Quit();
+    }
+}
